cc5bed9214d22fbfd2a404aead234be451cb33d6
Alice Horsey <alicehorsey@gmail.com>
2021-01-06 13:05:07 +0000

    installed and set up basic navigation

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Components/HomeScreen';
+import CreateProfile from './Components/CreateProfile';
+import AddPreferences from './Components/AddPreferences';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Ready to start working on our app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="CreateProfile" component={CreateProfile} options={{ title: 'Profile' }} />
+        <Stack.Screen name="AddPreferences" component={AddPreferences} options={{ title: 'Preferences' }} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
