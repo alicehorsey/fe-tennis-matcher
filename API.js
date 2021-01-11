4db9b1588a8f719638b566d951298cf95d92ad5e
Michael Squires <michaelsquires@hotmail.co.uk>
2021-01-11 13:44:54 +0000

    adds remove player from matches functionality

--- API.js
+++ API.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const tennisAPI = axios.create({ baseURL: "http://tennis-match-app.herokuapp.com" })
 
 export const getUsers = (currentUser) => {
-    const { gender, playing_hand, min_ability, max_ability, weekday_daytime, weekday_evening, weekends } = currentUser;
+    const { username, gender, playing_hand, min_ability, max_ability, weekday_daytime, weekday_evening, weekends } = currentUser;
     return tennisAPI
-        .get("/users")
+        .get("/users", { params: { username, gender, min_ability, max_ability } })
         .then(({ data }) => data.users)
         .catch((error) => console.log(error))
 }
 
-//{ params: { gender, playing_hand, min_ability, max_ability, weekday_daytime, weekday_evening, weekends } })
+//{ params: { username, gender, playing_hand, min_ability, max_ability, weekday_daytime, weekday_evening, weekends } })
--- Components/DisplayMatches.jsx
+++ Components/DisplayMatches.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, Button, ScrollView, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, Image, Button, ScrollView, Dimensions, StyleSheet, Alert } from 'react-native';
 import firebase from '../constants/Firebase';
 import { getUsers } from '../API.js';
 const { width } = Dimensions.get("window")
@@ -10,11 +10,6 @@ const DisplayMatches = () => {
     const [matchedUsers, setMatchedUsers] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [active, setActive] = useState(0)
-    const [images, setImages] = useState([
-        'https://firebasestorage.googleapis.com/v0/b/tennis-match-b1298.appspot.com/o/aliciamolik%40me.com.jpg?alt=media&token=2ee044b6-89a6-4f09-8e0c-724751e7ded7',
-        'https://firebasestorage.googleapis.com/v0/b/tennis-match-b1298.appspot.com/o/apero777%40hotmail.co.uk.jpg?alt=media&token=0afd8ac4-3664-4199-b055-5de043a3a2af',
-        'https://firebasestorage.googleapis.com/v0/b/tennis-match-b1298.appspot.com/o/aurelie.vedy%40yahoo.co.uk.jpg?alt=media&token=60fc9c2b-6d5d-45e1-9848-8ba0cf46fec2',
-    ])
     const [user, setUser] = useState({
         "user_id": "1",
         "username": "martina.hingis@yahoo.co.uk",
@@ -27,9 +22,9 @@ const DisplayMatches = () => {
         "ability": "1",
         "playing_hand": "right-handed",
         "club_membership": "Kirkstall Abbey Tennis Club",
-        "weekday_daytime": "TRUE",
-        "weekday_evening": "FALSE",
-        "weekends": "FALSE",
+        "weekday_daytime": "",
+        "weekday_evening": "",
+        "weekends": "",
         "description": "Tennis is mostly mental. Of course you must have a lot of physical skill but you can't play tennis well and not be a good thinker. You win or lose the match before you even go out there. Venus Williams",
         "distance": "10",
         "min_ability": "1",
@@ -37,13 +32,14 @@ const DisplayMatches = () => {
         "hand_preference": "",
         "min_age": "18",
         "max_age": "100",
-        "gender_preference": ""
+        "gender_preference": "f"
     })
 
 
     useEffect(() => {
         getUsers(user)
             .then(matchingUsers => {
+                console.log(matchingUsers[0], matchingUsers.length)
                 setIsLoading(false)
                 setMatchedUsers(matchingUsers)
             })
@@ -54,49 +50,64 @@ const DisplayMatches = () => {
         if (slide != active) setActive(slide)
     }
 
+    const handleClick = () => {
+        const newMatchedUsers = [...matchedUsers]
+        newMatchedUsers.splice(active, 1)
+        setMatchedUsers(newMatchedUsers)
+    }
+
     return (
-        <View style={style.container}>
-            <ScrollView
-                pagingEnabled
-                horizontal style={style.scroll}
-                onScroll={changeActive}
-                scrollEventThrottle={16}
-                showsHorizontalScrollIndicator={false}
-                style={style.scroll}
-            >
-                {
-                    images.map((image, index) => (
-                        <View key={index} style={{ flexDirection: 'column' }}>
-                            <Image
-                                source={{ uri: image }}
-                                style={style.image}
-                            />
-                            <Text style={{ alignSelf: 'center' }}>INFO ABOUT USER NUMBER {index + 1}</Text>
-                            <Button
-                                title='Remove player'
-                            />
-                        </View>
-                    ))
-                }
-            </ScrollView>
-            <View style={style.pagination}>
-                {
-                    images.map((_, i) => (
-                        <Text key={i} style={i == active ? style.pagingActiveText : style.pagingText}>⬤</Text>
-                    ))
-                }
-            </View>
-        </View >
+        isLoading ? <View>
+            <Text>LOADING</Text>
+        </View> :
+            <View style={style.container}>
+                <ScrollView
+                    pagingEnabled
+                    horizontal style={style.scroll}
+                    onScroll={changeActive}
+                    scrollEventThrottle={16}
+                    showsHorizontalScrollIndicator={false}
+                    style={style.scroll}
+                >
+                    {
+                        matchedUsers.map((matchedUser, index) => (
+                            <View key={index} style={{ flexDirection: 'column' }}>
+                                <Button
+                                    title='Remove player'
+                                    id={index}
+                                    onPress={handleClick}
+                                />
+                                <Image
+                                    source={{ uri: matchedUser.image_url }}
+                                    style={style.image}
+                                />
+                                <View style={style.userInfo}>
+                                    <Text style={{ fontWeight: 'bold', fontSize: width / 20 }}>{matchedUser.first_name}</Text>
+                                    <Text >{matchedUser.description}</Text>
+                                </View>
+                            </View>
+                        ))
+                    }
+                </ScrollView>
+                <View style={style.pagination}>
+                    {
+                        matchedUsers.map((_, i) => (
+                            <Text key={i} style={i == active ? style.pagingActiveText : style.pagingText}>⬤</Text>
+                        ))
+                    }
+                </View>
+            </View >
     );
 };
 
 const style = StyleSheet.create({
-    container: { marginTop: 10, width, height },
+    container: { marginTop: 0, width, height },
     scroll: { width, height },
     image: { width, height, resizeMode: 'cover', flex: 1 },
+    userInfo: { width, paddingLeft: 5, paddingRight: 5, alignItems: 'center' },
     pagination: { flexDirection: 'row', position: 'absolute', bottom: -30, alignSelf: 'center' },
-    pagingText: { fontSize: (width / 20), color: 'green', margin: 3 },
-    pagingActiveText: { fontSize: (width / 20), color: 'black', margin: 3 }
+    pagingText: { fontSize: (width / 35), color: 'green', margin: 3 },
+    pagingActiveText: { fontSize: (width / 35), color: 'black', margin: 3 }
 })
 
 export default DisplayMatches;
