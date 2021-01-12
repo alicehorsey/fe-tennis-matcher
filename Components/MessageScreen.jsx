c92215c5df9b843c24dfb87b0a9482f93f67d5aa
Alice Horsey <alicehorsey@gmail.com>
2021-01-12 12:38:45 +0000

    adds message to state on send

--- Components/MessageScreen.jsx
+++ Components/MessageScreen.jsx
@@ -10,23 +10,38 @@ class Chat extends React.Component {
         messages: [],
     }
 
+    componentWillMount() {
+        this.setState({
+            messages: [],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
 
     render() {
-
+        console.log(this.state.messages)
         const { matchedUser, user } = this.props.route.params;
+
+
 
         return (
             <>
-                <Text>{matchedUser.first_name}</Text>
-                <Text>{user.first_name}</Text>
-
+                <Text>Send a message to {matchedUser.first_name}!</Text>
                 <GiftedChat
                     messages={this.state.messages}
+                    renderAvatar={() => null}
+                    onSend={(messages) => this.onSend(messages)}
+                    user={{
+                        id: user.first_name,
+                    }}
                 />
             </>
-        )
+        );
     }
-
 }
 
 
