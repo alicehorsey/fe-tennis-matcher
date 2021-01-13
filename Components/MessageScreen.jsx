bc07af3e506f9606030ff48834fe0c8f326d79a0
Alice Horsey <alicehorsey@gmail.com>
2021-01-13 09:17:05 +0000

    comments out firebase messaging code

--- Components/MessageScreen.jsx
+++ Components/MessageScreen.jsx
@@ -12,92 +12,93 @@ class Chat extends React.Component {
         messages: [],
     }
 
-    componentWillMount() {
-        this.setState({
-            messages: [],
-        });
-    }
+    //For connecting to firebase....
+    // componentWillMount() {
+    //     this.setState({
+    //         messages: [],
+    //     });
+    // }
 
-    get ref() {
-        return firebase.database().ref('messages');
-    }
+    // get ref() {
+    //     return firebase.database().ref('messages');
+    // }
 
-    on = callback =>
-        this.ref
-            .limitToLast(20)
-            .on('child_added', snapshot => callback(this.parse(snapshot)));
+    // on = callback =>
+    //     this.ref
+    //         .limitToLast(20)
+    //         .on('child_added', snapshot => callback(this.parse(snapshot)));
 
-    parse = snapshot => {
-        const { timestamp: numberStamp, text, user } = snapshot.val();
-        const { key: _id } = snapshot;
+    // parse = snapshot => {
+    //     const { timestamp: numberStamp, text, user } = snapshot.val();
+    //     const { key: _id } = snapshot;
 
-        const timestamp = new Date(numberStamp);
+    //     const timestamp = new Date(numberStamp);
 
-        const message = {
-            _id,
-            timestamp,
-            text,
-            user,
-        };
-        return message;
-    }
+    //     const message = {
+    //         _id,
+    //         timestamp,
+    //         text,
+    //         user,
+    //     };
+    //     return message;
+    // }
 
-    off() {
-        this.ref.off();
-    }
+    // off() {
+    //     this.ref.off();
+    // }
 
-    get uid() {
-        return (firebase.auth().currentUser || {}).uid;
-    }
+    // get uid() {
+    //     return (firebase.auth().currentUser || {}).uid;
+    // }
 
-    get timestamp() {
-        return firebase.database.ServerValue.TIMESTAMP;
-    }
-
-
-    send = messages => {
-        for (let i = 0; i < messages.length; i++) {
-            const { text, user } = messages[i];
-
-            const message = {
-                text,
-                user,
-                timestamp: this.timestamp,
-            };
-            this.append(message);
-        }
-    };
-
-    append = message => this.ref.push(message);
+    // get timestamp() {
+    //     return firebase.database.ServerValue.TIMESTAMP;
+    // }
 
 
-    componentDidMount() {
+    // send = messages => {
+    //     for (let i = 0; i < messages.length; i++) {
+    //         const { text, user } = messages[i];
+
+    //         const message = {
+    //             text,
+    //             user,
+    //             timestamp: this.timestamp,
+    //         };
+    //         this.append(message);
+    //     }
+    // };
+
+    // append = message => this.ref.push(message);
 
 
-        this.on(message =>
-            this.setState(previousState => ({
-                messages: GiftedChat.append(previousState.messages, message),
-            }))
-        );
-    }
-    // 2.
-    componentWillUnmount() {
-        this.off();
-    }
+    // componentDidMount() {
 
-    get user() {
-        // Return our name and our UID for GiftedChat to parse
-        return {
-            name: this.props.route.params.user.first_name,
-            _id: firebase.shared.uid,
-        };
-    }
 
-    onSend(messages = []) {
-        this.setState((previousState) => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }));
-    }
+    //     this.on(message =>
+    //         this.setState(previousState => ({
+    //             messages: GiftedChat.append(previousState.messages, message),
+    //         }))
+    //     );
+    // }
+    // // 2.
+    // componentWillUnmount() {
+    //     this.off();
+    // }
+
+    // get user() {
+    //     // Return our name and our UID for GiftedChat to parse
+    //     return {
+    //         name: this.props.route.params.user.first_name,
+    //         _id: firebase.shared.uid,
+    //     };
+    // }
+
+    // onSend(messages = []) {
+    //     this.setState((previousState) => ({
+    //         messages: GiftedChat.append(previousState.messages, messages),
+    //     }));
+    // }
 
     render() {
 
@@ -107,22 +108,20 @@ class Chat extends React.Component {
             <>
                 <Text>Send a message to {matchedUser.first_name}!</Text>
 
-
-                <GiftedChat
+                {/* <GiftedChat
                     messages={this.state.messages}
                     onSend={this.send}
                     user={this.user}
-                />
+                /> */}
 
-
-                {/* <GiftedChat
+                <GiftedChat
                     messages={this.state.messages}
                     renderAvatar={() => null}
                     onSend={(messages) => this.onSend(messages)}
                     user={{
                         _id: user.first_name,
                     }}
-                /> */}
+                />
             </>
         );
     }
